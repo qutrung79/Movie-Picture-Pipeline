5d7c1fb2486fd11680de370c08e906b0c1999b01
qutrung79 <qutrung79@gmail.com>
2024-07-25 21:49:46 +0700

    Project4

--- setup/init.sh
+++ setup/init.sh
@@ -1,17 +1,36 @@
+
 #!/bin/bash
 set -e -o pipefail
 
+JQ_PATH="/c/Users/ADMIN/jq.exe"
+
+# Fetch IAM github-action-user ARN
 echo "Fetching IAM github-action-user ARN"
-userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
+userarn=$(aws iam get-user --user-name github-action-user | "$JQ_PATH" -r .User.Arn)
 
 # Download tool for manipulating aws-auth
 echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
-chmod +x aws-iam-authenticator
+curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_windows_amd64.exe -o aws-iam-authenticator.exe
 
-echo "Updating permissions"
-./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
+# Check if the aws-iam-authenticator is executable
+if [ ! -x "./aws-iam-authenticator.exe" ]; then
+  echo "Error: aws-iam-authenticator.exe is not executable or not found"
+  exit 1
+fi
 
+# Fetch current aws-auth ConfigMap
+current_config=$(kubectl get configmap aws-auth -n kube-system -o yaml)
+
+# Check if the user ARN already exists in the ConfigMap
+if echo "$current_config" | grep -q "$userarn"; then
+  echo "User ARN $userarn already exists in aws-auth ConfigMap. Skipping add user operation."
+else
+  # Update permissions
+  echo "Updating permissions"
+  ./aws-iam-authenticator.exe add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
+fi
+
+# Clean up
 echo "Cleaning up"
-rm aws-iam-authenticator
+rm aws-iam-authenticator.exe
 echo "Done!"
